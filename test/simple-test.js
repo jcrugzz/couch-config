1fa1c87a00181bc0008af3233973776705046fde
Jarrett Cruger <jcrugzz@gmail.com>
2014-02-25 03:04:38 -0500

    [test fix] add modifier function to test

--- test/simple-test.js
+++ test/simple-test.js
@@ -6,7 +6,14 @@ var config = require(path.join(__dirname, '..', 'config', 'config.json'));
 
 test('ensure this thing works and returns properly', function (t) {
   t.plan(1);
-
+  config.modifier = function (obj) {
+    return Object.keys(obj).reduce(function (acc, key) {
+      var pieces = obj[key].split('/');
+      pieces[pieces.length - 2] = 'app';
+      acc[key] = pieces.join('/');
+      return acc;
+    }, {});
+  }
   couchConfig(config, function (err) {
     if (err) {
       return t.fail(err);
